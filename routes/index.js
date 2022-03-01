733a38985a915ba89f110abd52ecdd43ffdc98c6
unknown <pedrogilsenarego@gmail.com>
2022-03-01 19:54:41 +0000

    a

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require ("express")
+const router = express.Router()
+
+
+// Login/Landing Page
+// GET/
+router.get("/", (req,res)=>{
+    res.render("login")
+})
+
+// DashBoard
+// GET/
+router.get("/dashboard", (req,res)=>{
+    res.render("dashboard")
+})
+
+module.exports=router
